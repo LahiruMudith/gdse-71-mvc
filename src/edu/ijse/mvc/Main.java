3fbb50978983636f1cb45f2bc2e024bac13002c9
LahiruMudith <lahimudith@gmail.com>
2024-09-11 16:28:12 +0530

    create customer new Pane

--- src/edu/ijse/mvc/Main.java
+++ src/edu/ijse/mvc/Main.java
@@ -7,10 +7,13 @@ package edu.ijse.mvc;
 import edu.ijse.mvc.db.DBConnection;
 import edu.ijse.mvc.view.ItemView;
 import edu.ijse.mvc.view.CustomerView;
+import edu.ijse.mvc.view.LayoutFrame;
+import java.awt.ComponentOrientation;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
 
 /**
  *
@@ -21,18 +24,22 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-        } catch (SQLException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        ItemView itemView = new ItemView();
-        itemView.setVisible(true);
-        CustomerView customerView = new CustomerView();
-        customerView.setVisible(true);
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        new LayoutFrame().setVisible(true);
+//        try {
+//            Connection connection = DBConnection.getInstance().getConnection();
+//        } catch (SQLException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        CustomerView customerView = new CustomerView();
+//        customerView.setLocationRelativeTo(null);
+//        customerView.setVisible(true);
+//        ItemView itemView = new ItemView();
+//        itemView.setLocationRelativeTo(null);
+//        itemView.setVisible(true);
+        
     }
     
 }
